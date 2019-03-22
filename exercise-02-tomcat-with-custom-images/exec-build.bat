38cf2c0377f9dab373440f0a38e676b3d3ef841c
pduleba <pduleba@pgs-soft.com>
2019-03-22 12:37:31 +0100

    Exercise 2 :: Build custom image running Tomcat with custom web pages

---
+++ exercise-02-tomcat-with-custom-images/exec-build.bat
@@ -0,0 +1,1 @@
+docker build -t pduleba-tomcat:latest .
---
+++ exercise-02-tomcat-with-custom-images/exec-run-tomcat-server.bat
@@ -0,0 +1,4 @@
+@echo off
+REM Once running enter http://localhost:8080/docker/index.html
+REM
+docker run --rm -i -t -p 8080:8080 pduleba-tomcat:latest
